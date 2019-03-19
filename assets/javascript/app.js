7f8cc637fd4a1ae06f94fa28e200376c640616a6
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2019-03-18 20:08:30 -0400

    directory skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// Before you can make any part of our site work, 
+// you need to create an array of strings, each one related to a topic 
+// that interests you. 
+// Save it to a variable called `topics`.
+const log = console.log;
+$(document).ready(function(){
+    log(`ready`);
+});

13d71c54981b9812cacc4321bca2277e9e50b49b
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2019-03-25 20:34:43 -0400

    added server to giph giving

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 app.use(express.static(__dirname + '/assets'));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });

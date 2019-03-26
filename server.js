181d6585530a0174593cf3f5bbd551e407a1bf6d
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2019-03-25 20:29:25 -0400

    added server to giphs_giving

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(PORT, () => { console.log(`server started on ${PORT}`) });

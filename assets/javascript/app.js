f332663ee76c1fb33a350d0b1294a7ff82b9813d
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2019-04-17 18:57:37 -0400

    logged steps

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,14 +15,32 @@ $(document).ready(() => {
 
     let topics = [`Nipsey Hussle`, `J Cole`, `Kendrick Lamar`, `Beyonce`, `Rihanna`];
 
+    log(`assigning/initializng variables:`);
+    // log(`1a | MY_KEY: ${MY_KEY}`);
+    log(`1b | API_KEY: ${API_KEY}`);
+    log(`1c | topics:${topics}`);
+    log(`-------------------------`);
+
+
     // jQuery.ajaxSetup({async:false});
     $.post("/", (data) => {
         MY_KEY = data.MY_KEY;
         API_KEY = data.API_KEY;
+        log(`2a | Inside post method..`)
+        // log(`2b | data: ${JSON.stringify(data)}`);
+        log(`-------------------------`);
 
         if (MY_KEY) {
+            log(`3a | Inside of post method within conditional statement..`);
+            // log(`3b | MY_KEY: ${MY_KEY}`);
+            log(`-------------------------`);
+
+            log(`4a | giphs are displaying...`);
+            log(`-------------------------`);
+
             displayGiphs(displayRandomTopic(topics));
-            jQuery.ajaxSetup({ async: false });
+
+            // jQuery.ajaxSetup({ async: false });
         }
 
     });
@@ -73,58 +91,43 @@ $(document).ready(() => {
     // this returns a random topic 
     function displayRandomTopic(topicsArray) {
         let randomTopic = Math.floor(Math.random() * topicsArray.length);
-        return topicsArray[randomTopic];
+        let topicPicked = topicsArray[randomTopic];
+        log(`5a | Inside displayRandomTopic function... `);
+        log(`5b | Topic: ${topicPicked}`);
+        log(`-------------------------`);
+        return topicPicked;
     }
 
     function displayGiphs(topicChosen) {
+        log(`6a | displayingGiphs function...`);
+        log(`6b | topic should still be...${topicChosen}`);
+        // log(`6c | MY_KEY: ${MY_KEY}`);
+        log(`6d | API_KEY: ${API_KEY}`);
+        log(`-------------------------`);
 
         if (MY_KEY) {
-            console.log(`display giphs..`);
-            console.log(API_KEY);
-
-            // https://api.giphy.com/v1/gifs/search?q=${topicChosen}&api_key=${MY_KEY}&limit=10/
-            // let stringsss = 'https://api.giphy.com/v1/gifs/search?q=' + topicChosen + '&' + 'api_key=' + MY_KEY + '&'+ 'limit=10/'
-
-            // fetch(stringsss)
-            //     .then(function (giphs) {
-            //         $('.giph_div').remove();
-
-            //         let list_of_giphs = giphs.data;
-
-            //         list_of_giphs.forEach((giph) => {
-            //             let image_still = giph.images.original_still.url;
-            //             let image_animated = giph.images.downsized.url;
-            //             let giph_rating = giph.rating;
-
-            //             // Giph Image
-            //             let image = $(`<img class='giph'>`);
-            //             image.attr('data-still', image_still);
-            //             image.attr('id', giph.id);
-            //             image.attr('data-animated', image_animated);
-            //             image.attr('data-status', 'paused')
-            //             image.attr('alt', giph.title);
-            //             image.attr('src', image_still);
-
-            //             let div = $(`<div class="d-inline-block p-2 text-black giph_div">`);
-            //             div.append(image);
-            //             div.append(`<p>Rated: ${giph_rating}</p>`);
-
-            //             $(`.giphs`).append(div);
-            //         });
-            //     });
-
-
+            log(`7a | if MY_KEY is not undefined then display giphs...`);
+            // log(`7b | MY_KEY: ${MY_KEY}`);
+            log(`7c | API_KEY: ${API_KEY}`);
+            log(`-------------------------`);
 
             if (topicChosen.indexOf(' ') >= 0) {
+                log(`8a | If topic has a space then replace with a +`)
                 topicChosen = topicChosen.replace(' ', '+');
-                log(topicChosen);
+                log(`8b | topic is now...${topicChosen}`);
+                log(`-------------------------`);
+
             } else {
-                log(topicChosen);
+                log(`8a | topic does not have a space...`)
+                log(`8b | topicChosen...${topicChosen}`);
+                log(`-------------------------`);
             }
 
 
             // RUNNER UP
             let queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + topicChosen + '&' + 'api_key=' + MY_KEY + '&' + 'limit=10/'
+            log(`9a | query url: ${queryURL}`);
+            log(`-------------------------`);
 
             $.get(queryURL, function (giphs) {
 
@@ -157,50 +160,7 @@ $(document).ready(() => {
                 });
 
             });
-            // RUNNER UP
-
-
-
-
-
-            // ORIGINAL COPY
-            // END
-            // $.ajax({
-            //     url: `https://api.giphy.com/v1/gifs/search?q=${topicChosen}&api_key=${MY_KEY}&limit=10`,
-            //     context: document.body
-            // }).done(function (giphs) {
-            //     // Note:
-            //     // downsized is moving giph
-            //     // original_still is the paused giph
-            //     $('.giph_div').remove();
-
-            //     // first giph = giphs.data[0].images
-            //     let list_of_giphs = giphs.data;
-
-            //     list_of_giphs.forEach((giph, i) => {
-            //         let image_still = giph.images.original_still.url;
-            //         let image_animated = giph.images.downsized.url;
-            //         let giph_rating = giph.rating;
-
-            //         // Giph Image
-            //         let image = $(`<img class='giph'>`);
-            //         image.attr('data-still', image_still);
-            //         image.attr('id', giph.id);
-            //         image.attr('data-animated', image_animated);
-            //         image.attr('data-status', 'paused')
-            //         image.attr('alt', giph.title);
-            //         image.attr('src', image_still);
-
-            //         let div = $(`<div class="d-inline-block p-2 text-black giph_div">`);
-            //         div.append(image);
-            //         div.append(`<p>Rated: ${giph_rating}</p>`);
-
-            //         $(`.giphs`).append(div);
-            //     });
-            // });
-            // FINISH
         }
-
     }
 
     $(document).on('click', 'img.giph', (event) => {
